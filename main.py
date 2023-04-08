5923dfb649fca27d54054f32cbf9aeffda5af4e2
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-04-08 17:23:09 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+# Быстрая сортировка
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    else:
+        pivot = array[0]
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+    return quick_sort(less) + [pivot] + quick_sort(greater)
+
+list1 = [10, 3, 5, 8, -1, 88, 1345, 3, 45]
+quick_sort(list1)
+print(list1)
+
+# Сортировка слияния
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j += 1
+            k += 1
+    
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k += 1
+        
+        while j < len(right):
+            nums[k] = right[j]
+            j +=1
+            k += 1
+
+list2 = [1,5,44,145,8,6,1,4,2,22,-3,54]
+merge_sort(list2)
+print(list2)
