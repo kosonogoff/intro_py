f048f5e2f8cd8f9c244017c30139f322563e683d
Pavel <60812654+kosonogoff@users.noreply.github.com>
2023-07-04 20:57:16 +0300

    add homework 007 How Many Catfish

---
+++ homework/007_HowMany.py
@@ -0,0 +1,22 @@
+# Даны две строки. Посчитайте сколько раз каждый символ первой строки встречается во второй
+# «one» «onetwonine» - o – 2, n – 3, e – 2
+
+string_first = input('Введите превую строку: ').lower()
+string_second = input('Введите вторую строку: ').lower()
+
+def how_many(string, symbol):
+    count = 0
+    for i in range(len(string)):
+        if symbol == string[i]:
+            count += 1
+    return count
+
+
+def print_symbol(one, two):
+    index = 0
+
+    while index < len(one):
+        print(f'Символ "{one[index]}" во второй строке встречается {(how_many(two, one[index]))} раз(а)')
+        index += 1
+
+print_symbol(string_first, string_second)
